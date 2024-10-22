2a152228c55bb4fab70e54cbe12560a13e645591
송치원 <iamchiwon@gmail.com>
2024-10-22 15:13:10 +0900

    Behind the pipeline

--- nlp/transformer.ch2.py
+++ nlp/transformer.ch2.py
@@ -9,3 +9,33 @@ raw_inputs = [
 ]
 inputs = tokenizer(raw_inputs, padding=True, truncation=True, return_tensors="pt")
 print(inputs)
+
+#----------
+
+from transformers import AutoModel
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+
+model = AutoModel.from_pretrained(checkpoint)
+outputs = model(**inputs)
+
+print(outputs.last_hidden_state.shape)
+
+#----------
+
+from transformers import AutoModelForSequenceClassification
+
+
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+outputs = model(**inputs)
+
+print(outputs.logits.shape)
+print(outputs.logits)
+
+#----------
+
+import torch
+
+predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+print(predictions)
+print(model.config.id2label)
